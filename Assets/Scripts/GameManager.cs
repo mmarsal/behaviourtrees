ecc6a7ac041b813558075e6f529198ad70bad7bb
Martin Marsal <mmarsal@stud.hs-heilbronn.de>
2024-12-29 17:08:36 +0100

    Add timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverUI;
+
+    public float timeRemaining = 180f;
+    public TextMeshProUGUI timerText;
+    private bool timerRunning = true;
+
+    void Update()
+    {
+        if (timerRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerDisplay();
+            }
+            else
+            {
+                timerText.text = "00:00";
+                timeRemaining = 0;
+                timerRunning = false;
+                TimerEnded();
+            }
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timerText.text = $"{minutes:00}:{seconds:00}"; // Format as MM:SS
+    }
+
+    void TimerEnded()
+    {
+        Debug.Log("Timer has ended!");
+    }
 
     public void GameOver()
     {
